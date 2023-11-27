983ae3a5bdcab183744408bf3e26f29a0c7f78a8
tomasdarienko <darienkotomas@gmail.com>
2023-11-27 20:35:53 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// creo costante lista
+const lista = ['farina', 'uova', 'pomodoro', 'olio', 'sale','pasta'];
+// assegno a cont list il valore dell id nel html
+const cont_list = document.getElementById('spesa');
+// inzializzo il contatore i
+let i=0;
+
+// creo un ciclo while che si ripete tante volte quanti sono gli elementi nella lista
+while ( i<=lista.length){
+     // creo li
+     const li = document.createElement ('li');
+     // inserisco li nel ul
+     cont_list.appendChild(li);
+     // inserisco nel li un elemento del array
+     li.innerText = lista[i];
+     // incremento
+     i++
+}
